0ed834ede7ea585f0b689201bae8701102d1864c
vince <vincent@djengo.net>
2012-07-11 12:30:12 +0200

    correction sur les helpers

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -1,29 +1,4 @@
 require 'yaml'
 
 module BlogHelper
-
-   def parse_yaml(h, indent)
-
-    return_phrase = ""
-
-    indent = indent + "--"
-
-    h.each_key do |key2|
-      if h[key2].class == Hash
-        # return_phrase << '<br />'
-        return_phrase << indent + "<br />"
-        return_phrase << indent + "| " + key2 << '<br />'
-        return_phrase << parse_yaml(h[key2], indent)
-      else
-        return_phrase << indent << "[" + key2.to_s + "] "        
-        return_phrase << h[key2].to_s
-        # return_phrase <<" est de type = > " +  h[key2].class.to_s
-        return_phrase << '<br />'
-      end
-    end
-
-    return_phrase.to_s
-  end
-
-
 end
