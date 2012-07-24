b4155e604d143d1dedb581c88feb250ea9a4d251
vince <vincent@djengo.net>
2012-07-24 11:17:21 +0200

    mise en place du in line avec David

--- app/controllers/translations_in_line_controller.rb
+++ app/controllers/translations_in_line_controller.rb
@@ -1,6 +1,8 @@
 class TranslationsInLineController < ApplicationController
 
   def index
+    puts("-----------------------------")
+    puts(params)
     @locale = current_translator.locale
     if @locale
       @translations = @locale.translations
@@ -11,13 +13,19 @@ class TranslationsInLineController < ApplicationController
   end
 
   def edit
-    @translations = Translation.all
+    @translations = Translation.find([:params])
   end
 
   def update
     Translation.update(params[:translations].keys, params[:translations].values)
     flash[:notice] = "Translations updated"
     redirect_to root_path
+  end
+
+  def create
+    @corresponding_translation = Translation.find(params[:translation_id])
+
+    create_respond_to(@corresponding_translation)
   end
 
 end
